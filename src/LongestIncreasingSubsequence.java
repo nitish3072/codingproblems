9e41686dbbc8202e675bd215d2dd69c4edfa23e2
Nitish Sharma <nitish.iitkgp09@gmail.com>
2018-11-13 23:45:58 +0530

    Adding new files

---
+++ src/LongestIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] ar1 = scanner.nextLine().split(",");
+        int[] ar = new int[ar1.length];
+        int[] computed = new int[ar1.length];
+        for(int i=0;i<ar1.length; i++) {
+            computed[i] = 1;
+            ar[i] = Integer.valueOf(ar1[i]);
+        }
+        for(int i=0;i<ar1.length;i++) {
+            int max = 0;
+            int value = 0;
+            for(int j=0; j<i;j++) {
+                if(computed[j]>max) {
+                    max = computed[j];
+                    value = ar[j];
+                }
+            }
+            if(value<ar[i]) {
+                computed[i] = 1 + max;
+            }
+        }
+        System.out.println(computed);
+    }
+}
---
+++ src/MedianSortedArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Median of two sorted arrays of same size
+ * There are 2 sorted arrays A and B of size n each.
+ * Write an algorithm to find the median of the array obtained after merging the above 2 arrays(i.e. array of length 2n).
+ * The complexity should be O(log(n)).
+ *
+ * Divide and Conquer
+ *
+ * Link: https://www.geeksforgeeks.org/median-of-two-sorted-arrays/
+ */
+
+
+public class MedianSortedArray {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] ar1 = scanner.nextLine().split(",");
+        String[] ar2 = scanner.nextLine().split(",");
+        
+    }
+}
