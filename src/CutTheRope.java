549af7aec70cb80f4ce14e647b9c1ad840f17d51
Nitish Sharma <nitish.iitkgp09@gmail.com>
2018-11-16 01:17:08 +0530

    Cut the rope start

---
+++ src/CutTheRope.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CutTheRope {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = scanner.nextLine().split(",");
+    }
+}
