8d71db5e3091644d0b8e982cd7e736aa6dd7b884
Nitish Sharma <nitish.iitkgp09@gmail.com>
2019-09-10 22:50:44 +0530

    Adding new problem of Merged Sorted Array

---
+++ src/MergeSortedArrays.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays/0/
+ */
+
+public class MergeSortedArrays {
+
+    public static void main(String[] args) {
+        Scanner sc = new java.util.Scanner(System.in);
+        int totalCases = sc.nextInt();
+        while (totalCases>0) {
+            sc.nextLine();
+            sc.nextLine();
+            String[] first = sc.nextLine().split(" ");
+            String[] second = sc.nextLine().split(" ");
+            List<Integer> firstList = new LinkedList<>();
+            List<Integer> secondList = new LinkedList<>();
+            for(String l: first) {firstList.add(Integer.valueOf(l));}
+            for(String l: second) {secondList.add(Integer.valueOf(l));}
+            int i = 0;
+            int j =0;
+            while (i<firstList.size()) {
+                if(firstList.get(i)>secondList.get(j)) {
+                    j++;
+                } else {
+                    secondList.add(j, firstList.get(i));
+                    i++;
+                    j++;
+                }
+            }
+            totalCases--;
+            secondList.stream().map(t -> t + " ").forEach(System.out::print);
+            System.out.println();
+        }
+
+    }
+}
