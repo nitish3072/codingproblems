4f1e93f1da4cf9506e461027a65ed3da5c74155a
Nitish Sharma <nitish.iitkgp09@gmail.com>
2026-03-23 07:12:24 +0530

    adding revision problems

---
+++ src/leetcode/practice_march2026/SubarraySumEqualsK.java
@@ -0,0 +1,26 @@
+package leetcode.practice_march2026;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+        int currSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            currSum += num;
+            if (map.containsKey(currSum - k)) {
+                count += map.get(currSum - k);
+            }
+            map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+}
